7adccf978af6e04de8d22848347df7b12ca3d56b
Rachel Xu <rachelxu.sauderfinance@gmail.com>
2021-01-25 18:55:01 -0800

    Updated app_HSV_control.r with html for bottom half of our app.

--- app_HSV_control.r
+++ app_HSV_control.r
@@ -9,6 +9,7 @@ app$layout(
     htmlDiv(
         className = "ten columns offset-by-half",
         list(
+            # row 1: app title and app description
             htmlDiv(
                 className = "row",
                 list(
@@ -22,6 +23,7 @@ app$layout(
                     )
                 ),
             ),
+            # row 2: 2 titles and control side by side
             htmlDiv(
                 className = "row",
                 list(
@@ -57,6 +59,7 @@ app$layout(
                     )
                 )
             ),
+            # row 3: 2 histograms side by side
             htmlDiv(
                 className = "row",
                 list(
@@ -80,6 +83,142 @@ app$layout(
                         ),
                         style = list('marginLeft' = 0)
                     )
+                )
+            ),
+            # row 4: 1 title on `Finding the Top 5 Similar Foundations by HSV`
+            htmlDiv(
+                className = "row",
+                list(
+                    htmlH5('Finding the Top 5 Similar Foundations by HSV:',
+                            className = "six columns"
+                    )
+                )
+            ),
+            # row 5: 3 titles and 3 sliders control underneath
+            htmlDiv(
+                className = "row",
+                list(
+                    htmlDiv(
+                        className = "three columns",
+                        list(
+                            htmlH6("Hue (Color Spectrum)"),
+                            dccSlider(
+                                id = "slider_hue",
+                                min = 0, 
+                                max = 50, 
+                                step = 1,
+                                value = 29,
+                                marks = list(
+                                    "0" = '0°',
+                                    "10" = '10°',
+                                    "20" = '20°',
+                                    "30" = '30°', 
+                                    "40" = '40°',
+                                    "50" = '50°'
+                                )
+                            )
+                        )
+                    ),
+                    htmlDiv(
+                        className = "three columns",
+                        list(
+                            htmlH6("Saturation (Color Intensity)"),
+                            dccSlider(
+                                id = "slider_saturation",
+                                min = 0,
+                                max = 100,
+                                step = 1,
+                                value = 53,
+                                marks = list(
+                                    "0" = '0%',
+                                    "20" = '20%',
+                                    "40" = '40%',
+                                    "60" = '60%', 
+                                    "80" = '80%',
+                                    "100" = '100%'                                    
+                                )
+                            )
+                        )
+                    ),
+                    htmlDiv(
+                        className = "three columns",
+                        list(
+                            htmlH6("Value (Color Brightness)"),
+                            dccSlider(
+                                id = "slider_value_brightness",
+                                min = 20,
+                                max = 100,
+                                step = 1,
+                                value = 70,
+                                marks = list(
+                                    "20" = '20%',
+                                    "60" = '60%',
+                                    "100" = '100%'                                    
+                                )                                
+                            )
+                        )
+                    )
+                )
+            ),
+            # row 6: Selected HSV value and 1 subtitle on `Most Matching Results from Best Selling Foundation Lists` 
+            htmlDiv(
+                className = "row",
+                list(
+                    htmlDiv(
+                        className = "six columns",
+                        list(
+                            htmlP(id = "HSV_user_choice_output"),
+                            htmlDiv(id = "user_selected_color_div"),
+                            htmlH6("Most Matching Results from Best Selling Foundation Lists:")
+                        )
+                    )
+                )   
+            ),
+            # row 7: 5 top similar products
+            htmlBr(),
+            htmlDiv(
+                className = "row",
+                list(
+                    htmlDiv(
+                        className = "two columns",
+                        list(
+                            htmlH1(id = "match_1"),
+                            htmlP(id = "match_1_color"),
+                            htmlP(id = "match_1_text")
+                        )
+                    ),
+                    htmlDiv(
+                        className = "two columns offset-by-half",
+                        list(
+                            htmlH1(id = "match_2"),
+                            htmlP(id = "match_2_color"),
+                            htmlP(id = "match_2_text")
+                        )
+                    ),
+                    htmlDiv(
+                        className = "two columns offset-by-half",
+                        list(
+                            htmlH1(id = "match_3"),
+                            htmlP(id = "match_3_color"),
+                            htmlP(id = "match_3_text")
+                        )
+                    ),
+                    htmlDiv(
+                        className = "two columns offset-by-half",
+                        list(
+                            htmlH1(id = "match_4"),
+                            htmlP(id = "match_4_color"),
+                            htmlP(id = "match_4_text")
+                        )
+                    ),
+                    htmlDiv(
+                        className = "two columns offset-by-half",
+                        list(
+                            htmlH1(id = "match_5"),
+                            htmlP(id = "match_5_color"),
+                            htmlP(id = "match_5_text")
+                        )
+                    )                                       
                 )
             )
         )
