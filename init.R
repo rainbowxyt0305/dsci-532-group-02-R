084bc8206349dfb4d4bbab646fc8724a06b09e24
KangboLu <kblu@ucdavis.edu>
2021-01-30 13:15:09 -0800

    update docker file and init.R

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'colorspace', 'tidyverse', 'plotly', 'ggplot2'))
+install.packages(c('dash', 'here', 'ggthemes', 'remotes', 'colorspace', 'tidyverse', 'plotly', 'ggplot2'))
 # remotes::install_github('facultyai/dash-bootstrap-components@r-release')
